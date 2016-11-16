bce433fd5b5ff45e199b1e30fa95bb8c07046597
Michelle Hettinger <Michelle Hettinger>
2016-11-16 18:09:59 -0500

    css style changes and now typing 2 and 3 letters further reduces the number of displayed elements

--- ElementSelector.js
+++ ElementSelector.js
@@ -18,58 +18,64 @@ export default class ElementSelector extends Component {
 			var listElements2 = [];
 			var listElements3 = [];
 
+			// Loop through every typed letter
+			for (i=0; i<userInput.length; i++){
 
-			//If the user has typed in one letter
-			//Empty out the listElements array
-			//Loop through all elements,
-			//If the first letter in the elements acronym OR the elements name is equal to the user input,
-			//push that into the listElements array
-			//Log out the list and return it
+				if (i==0){
+					//Loop through all elements
+					for (j=0; j<ElementsArray.length;j++){
+						//If the letters at position i match, push that element to the array
+						if (userInput.charAt(i) == ElementsArray[j].elementName.charAt(i).toLowerCase() || userInput.charAt(i) == ElementsArray[j].elementAcronym.charAt(i).toLowerCase()){
+							listElements.push(ElementsArray[j]);
+						}	
+					}					
+				}
 
-			//The else's follow the same logic, except the matching element comes from the listElements array
-			//and is pushed into the listElements2 array (for 2 letters).
+				else if (i==1){
+					//Loop through the first list of elements
+					for (j=0; j<listElements.length;j++){
+						//If the letters at position i match, push that element to a new array
+						if (userInput.charAt(i) == listElements[j].elementName.charAt(i).toLowerCase() || userInput.charAt(i) == listElements[j].elementAcronym.charAt(i).toLowerCase()){
+							listElements2.push(listElements[j]);
+						}	
+					}
+				}
 
+				else if (i==2){
+					//Loop through the second list of elements
+					for (j=0; j<listElements2.length;j++){
+						//If the letters at position i match, push that element to a new array
+						if (userInput.charAt(i) == listElements2[j].elementName.charAt(i).toLowerCase() || userInput.charAt(i) == listElements2[j].elementAcronym.charAt(i).toLowerCase()){
+							listElements3.push(listElements2[j]);
+						}	
+					}
+				}
+
+				
+			}
+
+			//Depending on how many letters were typed in, display the appropriate array
 			if (userInput.length == 0){
 				return listElements;
 			}
-
 			else if (userInput.length == 1){
-				listElements = [];
-				for (i=0; i<ElementsArray.length;i++){
-					if (userInput.charAt(0) == ElementsArray[i].elementName.charAt(0).toLowerCase() || userInput.charAt(0) == ElementsArray[i].elementAcronym.charAt(0).toLowerCase()){
-						listElements.push(ElementsArray[i]);
-					}
-				}
 				console.log(listElements);
 				return listElements;
 			}
-
 			else if (userInput.length == 2){
-				listElements2 = [];
-				for (i=0;i<listElements.length;i++){
-					if (userInput.charAt(1) == listElements[i].elementName.charAt(1) || userInput.charAt(1) == listElements[i].elementAcronym.charAt(1)){
-						listElements2.push(listElements[i]);
-					}
-				}
 				console.log(listElements2);
 				return listElements2;
 			}
-
 			else if (userInput.length == 3){
-				listElements3 = [];
-				for (i=0;i<listElements2.length;i++){
-					if (userInput.charAt(2) == listElements2[i].elementName.charAt(2)){
-						listElements3.push(listElements2[i]);
-					}
-				}
 				console.log(listElements3);
 				return listElements3;
 			}
+			else if (userInput.length >= 4){
+				return listElements3;
+			}
+
 		}
 
-
-		//Just trying to see where i can find the mobile debug log.
-		console.log("hello");
 
 		//Map through the array of elements found and display them
 		var elementsFound = findElements(this.props.userInput).map(function(element, i){
@@ -79,40 +85,56 @@ export default class ElementSelector extends Component {
 					<Text style={styles.elementNumber} key={i}>{element.atomicNumber}</Text>
 					<Text style={styles.elementAcronym}>{element.elementAcronym}</Text>
 					<Text style={styles.elementName}>{element.elementName}</Text>
-					<Text style={styles.elementMass}>{element.mass}</Text>
+					<Text style={styles.elementMass}>{element.mass.toFixed(3)}</Text>
 				</View>
 			)
 		})
 
 		//Render the elementsFound 'div'
 		return (
-			<View>
+			<View style={styles.allElements}>
 		       {elementsFound}
 	        </View>
 		)
+	
 	}
 }
 
 const styles = StyleSheet.create({
 
+	allElements: {
+		marginLeft: 8,
+		flexWrap: 'wrap',
+		flexDirection: 'row',
+	},
+
+
 	elementDiv:{
-		width: 100,
+		width: 80,
+		height: 80,
+		marginLeft: 5,
+		marginBottom: 5,
+
 		borderRadius: 4,
-		borderWidth: 0.75,
+		borderWidth: 1,
 		borderColor: 'black',
 
 	},
 	elementNumber: {
-
+		fontSize: 10,
+		marginLeft: 2,
 	},
 	elementAcronym: {
-
+		textAlign: "center",
+		fontSize: 18,
 	},
 	elementName: {
-
+		textAlign: "center",
+		fontSize: 15,
 	},
 	elementMass: {
-
+		textAlign: "center",
+		fontSize: 12,
 	}
 
 });
