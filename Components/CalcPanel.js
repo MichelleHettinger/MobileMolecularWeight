2400bfcb872f83a7efadd4e136421004d2b33449
Michelle Hettinger <Michelle Hettinger>
2017-01-06 17:40:07 -0500

    tabs to spaces

--- Components/CalcPanel.js
+++ Components/CalcPanel.js
@@ -6,44 +6,44 @@ const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 export default class ElementCalculator extends Component {
-	constructor(props) {
-		super(props);
+constructor(props) {
+  super(props);
 
-		this.getPlusMinus = this.getPlusMinus.bind(this);
-    this.displayElements = this.displayElements.bind(this);
-	}
-	getPlusMinus(input, element, i) {
-    //console.log(input)
-		this.props.newEdit(input, element, i);
-	}
-  displayElements(selectedElements){
-    return selectedElements.map( (element, i) => {
-      return (
-        <View key={i} style={[styles.elementDiv]}>
-          <Button key={i} onPress={()=>this.getPlusMinus('+', element, i)} style={[styles.plus]}> + </Button>
+  this.getPlusMinus = this.getPlusMinus.bind(this);
+  this.displayElements = this.displayElements.bind(this);
+}
+getPlusMinus(input, element, i) {
+  //console.log(input)
+  this.props.newEdit(input, element, i);
+}
+displayElements(selectedElements){
+  return selectedElements.map( (element, i) => {
+    return (
+      <View key={i} style={[styles.elementDiv]}>
+        <Button key={i} onPress={()=>this.getPlusMinus('+', element, i)} style={[styles.plus]}> + </Button>
 
-          <Text style={[styles.acronym]}>
+        <Text style={[styles.acronym]}>
 
-            {element.elementAcronym}
+          {element.elementAcronym}
 
-            <Text key={i} style={[styles.subscript]}>
-              {this.props.elementMultipliers[i]}
-            </Text>
-
+          <Text key={i} style={[styles.subscript]}>
+            {this.props.elementMultipliers[i]}
           </Text>
 
-          <Button onPress={()=>this.getPlusMinus("-", element, i)} style={[styles.minus]}> - </Button>
-        </View>
-      )
-    })
-  }
+        </Text>
 
-	render(){
+        <Button onPress={()=>this.getPlusMinus("-", element, i)} style={[styles.minus]}> - </Button>
+      </View>
+    )
+  })
+}
+
+  render(){
 
     const user = this.props.user;
 
-		// Upon tapping a selected atom, loop all atoms
-		const elementsToDisplay = this.displayElements(this.props.selectedElements);
+    // Upon tapping a selected atom, loop all atoms
+    const elementsToDisplay = this.displayElements(this.props.selectedElements);
 
     if (user){
       return (
@@ -61,76 +61,76 @@ export default class ElementCalculator extends Component {
       )
     }
 
-		return (
-			<View style={[styles.calculationPanel]}>
-				<View style={[styles.MWTView]}>
-					<Text style={[styles.MWTText]}>
-						Molecular Weight: {this.props.total.toFixed(3)} g/mol
-					</Text>
-				</View>
+    return (
+      <View style={[styles.calculationPanel]}>
+        <View style={[styles.MWTView]}>
+          <Text style={[styles.MWTText]}>
+            Molecular Weight: {this.props.total.toFixed(3)} g/mol
+          </Text>
+        </View>
 
-				<View style={[styles.elementRows]}>
-					{elementsToDisplay}
-				</View>
-			</View>
-		)
-	}
+        <View style={[styles.elementRows]}>
+          {elementsToDisplay}
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-	calculationPanel: {
-		height: height*0.28,
-		width: width*0.96,
+  calculationPanel: {
+    height: height*0.28,
+    width: width*0.96,
 
-		borderRadius: 4,
-		borderWidth: 1,
-		borderColor: 'black',
-	},
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
 
-	MWTView: {
-		height: height*0.031,
-		width: width*0.96,
-	},
+  MWTView: {
+    height: height*0.031,
+    width: width*0.96,
+  },
 
-	MWTText: {
-		textAlign: "center",
-		color: 'black',
-	},
+  MWTText: {
+    textAlign: "center",
+    color: 'black',
+  },
 
-	elementRows: {
-		flexWrap: 'wrap',
-		flexDirection: 'row',
+  elementRows: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
 
-		marginLeft: width*0.033, 
-	},
+    marginLeft: width*0.033, 
+  },
 
-	elementDiv: {
-		width: width*0.11,
-		height: height*0.11,
+  elementDiv: {
+    width: width*0.11,
+    height: height*0.11,
 
-		marginTop: height*0.01,
-		marginBottom: 0,
-	},
+    marginTop: height*0.01,
+    marginBottom: 0,
+  },
 
-	plus: {
-		fontSize: height*0.022,
-	},
+  plus: {
+    fontSize: height*0.022,
+  },
 
-	minus: {
-		color: 'red',
-		fontSize: height*0.022,
-	},
+  minus: {
+    color: 'red',
+    fontSize: height*0.022,
+  },
 
-	acronym: {
-		color: 'black',
-		textAlign: 'center',
-		fontSize: height*0.03,
- 	},
+  acronym: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: height*0.03,
+  },
 
-	subscript: {
-		color: 'black',
-		fontWeight: 'bold',
+  subscript: {
+    color: 'black',
+    fontWeight: 'bold',
 
-		fontSize: height*0.015,
-	},
+    fontSize: height*0.015,
+  },
 });
--- index.android.js
+++ index.android.js
@@ -14,40 +14,40 @@ const height = Dimensions.get('window').height;
 //Set up and initialize Firebase
 import * as firebase from 'firebase';
 const config = {
-	apiKey: "AIzaSyBQUQPgITUNyCSsjufVVhJp-4laWw21QdU",
-	authDomain: "mobile-molecular-weight-85984.firebaseapp.com",
-	databaseURL: "https://mobile-molecular-weight-85984.firebaseio.com",
-	storageBucket: "mobile-molecular-weight-85984.appspot.com",
-	messagingSenderId: "837319764944"
+  apiKey: "AIzaSyBQUQPgITUNyCSsjufVVhJp-4laWw21QdU",
+  authDomain: "mobile-molecular-weight-85984.firebaseapp.com",
+  databaseURL: "https://mobile-molecular-weight-85984.firebaseio.com",
+  storageBucket: "mobile-molecular-weight-85984.appspot.com",
+  messagingSenderId: "837319764944"
 };
 
 //Only one instance of firebase can run at a time
 firebase.initializeApp(config);
 
 class ChemistryApp extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			text: '',
+    this.state = {
+      text: '',
       elementsFound: [],
-			elements: [], multipliers:[],
-			total: 0,
+      elements: [], multipliers:[],
+      total: 0,
 
-			email: '',
-			password: '',
+      email: '',
+      password: '',
 
-			newEmail: '',
-			newUsername: '',
+      newEmail: '',
+      newUsername: '',
 
       user: {},
       userSavedCompounds: {},
 
-			logged: false,
-			
-			loginModalVisible: false,
-			accountModalVisible: false,
-		};
+      logged: false,
+      
+      loginModalVisible: false,
+      accountModalVisible: false,
+    };
 
     //LoginModal methods
     this.grabUserEmail = this.grabUserEmail.bind(this);
@@ -63,14 +63,14 @@ class ChemistryApp extends Component {
     this.resetPassword = this.resetPassword.bind(this);
 
     //Methods getting data from other components
-		this.getKeypress = this.getKeypress.bind(this);
+    this.getKeypress = this.getKeypress.bind(this);
     this.findElements = this.findElements.bind(this);
-		this.getElement = this.getElement.bind(this);
-		this.getEdit = this.getEdit.bind(this);
+    this.getElement = this.getElement.bind(this);
+    this.getEdit = this.getEdit.bind(this);
 
     //Methods for header when logged in
     this.loggedHeader = this.loggedHeader.bind(this);
-		this.logout = this.logout.bind(this);
+    this.logout = this.logout.bind(this);
 
     //Methods for header when not logged in
     this.notLoggedHeader = this.notLoggedHeader.bind(this);
@@ -79,7 +79,7 @@ class ChemistryApp extends Component {
     this.keyboardRend = this.keyboardRend.bind(this);
     this.elemSelectorRend = this.elemSelectorRend.bind(this);
     this.calcPanelRend = this.calcPanelRend.bind(this);
-	}
+  }
 
   //LoginModal methods
   grabUserEmail(userEmail){
@@ -126,6 +126,8 @@ class ChemistryApp extends Component {
         });
       }
     });
+
+
   }
   signup(){
     firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
@@ -141,6 +143,7 @@ class ChemistryApp extends Component {
       console.log(newUserData);
       this.setLoginModalVisible(false);
 
+ 
     });
   }
 
@@ -216,25 +219,25 @@ class ChemistryApp extends Component {
   }
 
   //Methods getting data from other components
-	getKeypress(newText){
-		//console.log("------------------------------------------");
-		//console.log("User pressed: " + newText);
+  getKeypress(newText){
+    //console.log("------------------------------------------");
+    //console.log("User pressed: " + newText);
 
     let totalText = '';
 
-		if (newText == '<-'){
-			totalText = this.state.text.slice(0, -1);
-		}
-		else {
-			totalText = this.state.text + newText;
-		}
+    if (newText == '<-'){
+      totalText = this.state.text.slice(0, -1);
+    }
+    else {
+      totalText = this.state.text + newText;
+    }
 
-		this.setState({
-			text: totalText,
-		}, () => {this.findElements(totalText)} )
+    this.setState({
+      text: totalText,
+    }, () => {this.findElements(totalText)} )
 
-		//console.log(this.state)
-	}
+    //console.log(this.state)
+  }
   findElements (userInput) {
     //Find the right elements, then setState for found elements.
 
@@ -305,11 +308,11 @@ class ChemistryApp extends Component {
       })
     }
   }
-	getElement(newElement){
-		//console.log("------------------------------------------");
-		//console.log("User selected " + newElement.elementName);
+  getElement(newElement){
+    //console.log("------------------------------------------");
+    //console.log("User selected " + newElement.elementName);
 
-		// Push the element and multiplier into their respective arrays
+    // Push the element and multiplier into their respective arrays
     let currentElements = this.state.elements;
     let currentMultipliers = this.state.multipliers;
     let currentTotal = this.state.total;
@@ -318,44 +321,44 @@ class ChemistryApp extends Component {
     currentMultipliers.push(1);
     currentTotal += newElement.mass;
 
-		this.setState({
-			total: currentTotal,
-			elements: currentElements,
-			multiplier: currentMultipliers
-		});
+    this.setState({
+      total: currentTotal,
+      elements: currentElements,
+      multiplier: currentMultipliers
+    });
 
-		//console.log(this.state);
-	}
-	getEdit(input, element, i){
-		console.log("------------------------------------------");
-		console.log(input + " one " + element.elementName + " at position: " + i);
+    //console.log(this.state);
+  }
+  getEdit(input, element, i){
+    console.log("------------------------------------------");
+    console.log(input + " one " + element.elementName + " at position: " + i);
 
-		console.log(i)
+    console.log(i)
 
-		if (input == '+'){
-			this.state.multipliers[i] += 1;
-			this.state.total += element.mass;
-		}
-		else if (input == '-'){
-			this.state.multipliers[i] -= 1;
-			this.state.total -= element.mass;
-		}
+    if (input == '+'){
+      this.state.multipliers[i] += 1;
+      this.state.total += element.mass;
+    }
+    else if (input == '-'){
+      this.state.multipliers[i] -= 1;
+      this.state.total -= element.mass;
+    }
 
-		for (let j=0; j<this.state.multipliers.length; j++){
-			if (this.state.multipliers[j] == 0){
-				this.state.multipliers.splice(j, 1);
-				this.state.elements.splice(j, 1);
-			}
-		}
+    for (let j=0; j<this.state.multipliers.length; j++){
+      if (this.state.multipliers[j] == 0){
+        this.state.multipliers.splice(j, 1);
+        this.state.elements.splice(j, 1);
+      }
+    }
 
-		this.setState({
-			total: this.state.total,
-			elements: this.state.elements,
-			multiplier: this.state.multipliers
-		})
+    this.setState({
+      total: this.state.total,
+      elements: this.state.elements,
+      multiplier: this.state.multipliers
+    })
 
-		console.log(this.state);
-	}
+    console.log(this.state);
+  }
 
   //Methods for header when logged in
   loggedHeader(){
@@ -476,21 +479,21 @@ class ChemistryApp extends Component {
       ) 
     }
   }
-	logout(){
-		firebase.auth().signOut().catch(function(error){
+  logout(){
+    firebase.auth().signOut().catch(function(error){
 
-			alert("Error " + error);
+      alert("Error " + error);
 
-		}).then(function() {
+    }).then(function() {
 
-			this.setState({
-				logged: false,
-			});
+      this.setState({
+        logged: false,
+      });
 
-			alert("You have signed out");
+      alert("You have signed out");
 
-		}.bind(this))
-	}
+    }.bind(this))
+  }
 
   //Methods for header when not logged in
   notLoggedHeader(){
@@ -601,217 +604,217 @@ class ChemistryApp extends Component {
     )
   }
 
-	render() {
+  render() {
 
     const notLogged = this.notLoggedHeader();
     const keyboard = this.keyboardRend();
     const selector = this.elemSelectorRend();
     const calcpanel = this.calcPanelRend();
 
-		//If user is logged in
-		if (this.state.logged){
+    //If user is logged in
+    if (this.state.logged){
       const logged = this.loggedHeader();
-			return (
-				<View style={[styles.main]}>
+      return (
+        <View style={[styles.main]}>
             {logged}
             {selector}
             {calcpanel}
             {keyboard}
-				</View>
-			)
-		}
+        </View>
+      )
+    }
 
-		//Else if no one is logged in
-		return (
-			<View style={[styles.main]}>
+    //Else if no one is logged in
+    return (
+      <View style={[styles.main]}>
           {notLogged}
           {selector}
           {calcpanel}
           {keyboard}
-			</View>
-		)
-	}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
 
-	//	Account Modal //
+  //  Account Modal //
   accountModalContent: {
-  	marginTop: height*0.01,
-  	marginLeft: 0,
+    marginTop: height*0.01,
+    marginLeft: 0,
   },
-	accountModalHeading: {
-  	marginBottom: height*0.01,
+  accountModalHeading: {
+    marginBottom: height*0.01,
 
-		borderRadius: 4,
-		borderWidth: 1,
-		borderColor: 'black',
-	},
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
   accountModalText: {
-  	color: 'black',
-  	marginLeft: width*0.06,
+    color: 'black',
+    marginLeft: width*0.06,
   },
   accountTextInput: {
-  	width: width*0.6,
-  	height: height*0.09,
-  	marginBottom: 0,
+    width: width*0.6,
+    height: height*0.09,
+    marginBottom: 0,
 
-  	top: -1*height*0.03,
+    top: -1*height*0.03,
   },
   accountAuthFields: {
-	 marginLeft: width*0.12,
-	 marginBottom: height*0.01,
-	 width: width*0.7,
+   marginLeft: width*0.12,
+   marginBottom: height*0.01,
+   width: width*0.7,
   },
-	accountEmail: {
-		marginLeft:0,
-	},
-	accountUsername:{
-		marginBottom: 0,
-	},
-	newEmail: {
-		marginLeft: 0,
-	},
-	newusername: {
+  accountEmail: {
+    marginLeft:0,
+  },
+  accountUsername:{
+    marginBottom: 0,
+  },
+  newEmail: {
+    marginLeft: 0,
+  },
+  newusername: {
 
-	},
-	accountModalButtons: {
-		flexWrap: 'wrap',
-		flexDirection: 'row',
+  },
+  accountModalButtons: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
 
-		height: height*0.04,
-		width: width*0.35,
-		marginLeft: width*0.32,
-	},
-	submitAccountView: {
-		width: width*0.13,
-		marginLeft: width*0.045,
-	},
-	cancelAccountView: {
-		width: width*0.11,
-		marginRight: width*0.045,
-	},
-	resetPasswordView: {
-		width: width*0.35,
-		marginLeft: width*0.31,
-		marginBottom: height*0.05,
+    height: height*0.04,
+    width: width*0.35,
+    marginLeft: width*0.32,
+  },
+  submitAccountView: {
+    width: width*0.13,
+    marginLeft: width*0.045,
+  },
+  cancelAccountView: {
+    width: width*0.11,
+    marginRight: width*0.045,
+  },
+  resetPasswordView: {
+    width: width*0.35,
+    marginLeft: width*0.31,
+    marginBottom: height*0.05,
 
-		borderRadius: 4,
-		borderWidth: 1,
-		borderColor: 'black',
-	},
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
 
 
 
   main: {
-  	marginTop: height*0.01,
-  	marginLeft: width*0.02,
+    marginTop: height*0.01,
+    marginLeft: width*0.02,
   },
 
   //Both headers
   header: {
-	width: width*0.96,
-	height: height*0.08,
+  width: width*0.96,
+  height: height*0.08,
 
-	borderRadius: 4,
-	borderWidth: 1,
-	borderColor: 'black',
+  borderRadius: 4,
+  borderWidth: 1,
+  borderColor: 'black',
   },
   headerTitle: {
-  	textAlign: 'center',
-  	color: 'black',
-  	fontSize: width*0.05,
+    textAlign: 'center',
+    color: 'black',
+    fontSize: width*0.05,
   },
   headerButtonText: {
-  	textAlign: 'center',
-  	fontSize: height*0.02
+    textAlign: 'center',
+    fontSize: height*0.02
   },
 
   //Both modals
   modalTitle: {
-  	textAlign: 'center',
-  	fontSize: height*0.045,
-  	color: 'black',
+    textAlign: 'center',
+    fontSize: height*0.045,
+    color: 'black',
   },
   loginModalText: {
-  	color: 'black',
-  	marginLeft: width*0.06,
+    color: 'black',
+    marginLeft: width*0.06,
   },
 
   //View tag for login/signup button
   loginHeaderButton: {
-  	width: width*0.25,
-  	marginLeft: width*0.35,
+    width: width*0.25,
+    marginLeft: width*0.35,
   },
 
   //View tag for everything in the login modal
   loginModalContent: {
-  	marginTop: height*0.01,
-  	marginLeft: 0,
+    marginTop: height*0.01,
+    marginLeft: 0,
 
 
-	// borderRadius: 4,
-	// borderWidth: 1,
-	// borderColor: 'black',
+  // borderRadius: 4,
+  // borderWidth: 1,
+  // borderColor: 'black',
   },
 
   //View for the heading with login/signup button
   loginModalHeading: {
-  	marginBottom: height*0.05,
+    marginBottom: height*0.05,
 
-	borderRadius: 4,
-	borderWidth: 1,
-	borderColor: 'black',
+  borderRadius: 4,
+  borderWidth: 1,
+  borderColor: 'black',
   },
 
   //View tag for each auth input element
   loginAuthFields: {
-	marginLeft: width*0.15,
-	marginBottom: height*0.01,
+  marginLeft: width*0.15,
+  marginBottom: height*0.01,
   },
   modalAuth: {
-  	flexWrap: 'wrap',
-  	flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
 
-	width: width*0.85,
-	height: height*0.055,
+  width: width*0.85,
+  height: height*0.055,
 
   },
 
   //The view tags for input field
   modalEmail: {
-  	marginLeft: width*0.16,
+    marginLeft: width*0.16,
   },
   modalPass: {
-  	marginLeft: width*0.089,
+    marginLeft: width*0.089,
   },
 
   //Text box
   loginTextInput: {
-  	width: width*0.6,
-  	height: height*0.09,
-  	marginBottom: 0,
+    width: width*0.6,
+    height: height*0.09,
+    marginBottom: 0,
 
-  	top: -1*height*0.03,
+    top: -1*height*0.03,
 
-	// borderRadius: 4,
-	// borderWidth: 1,
-	// borderColor: 'black',
+  // borderRadius: 4,
+  // borderWidth: 1,
+  // borderColor: 'black',
   },
 
   //Log in
   loginModalButtons:{
-  	flexWrap: 'wrap',
-  	flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
 
-  	width: width*0.3,
-  	height: height*0.05,
-  	marginLeft: width*0.33,
+    width: width*0.3,
+    height: height*0.05,
+    marginLeft: width*0.33,
 
 
-	// borderRadius: 4,
-	// borderWidth: 1,
-	// borderColor: 'black',
+  // borderRadius: 4,
+  // borderWidth: 1,
+  // borderColor: 'black',
 
   },
 
@@ -820,28 +823,28 @@ const styles = StyleSheet.create({
 
   },
   cancelLoginView: {
-  	marginLeft: width*0.08,
+    marginLeft: width*0.08,
   },
   signupView:{
-  	marginRight: width*0.03,
+    marginRight: width*0.03,
   },
 
 
   //View tag for logged in header buttons
   accountButtons: {
-  	flexWrap: 'wrap',
-  	flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
 
-  	height: height*0.05,
+    height: height*0.05,
   },
-	myAccountButtonView: {
-		width: width*0.23,
-		marginLeft: width*0.375,
-	},
-	logoutButtonView: {
-		width: width*0.15,
-		marginLeft: width*0.165,
-	},
+  myAccountButtonView: {
+    width: width*0.23,
+    marginLeft: width*0.375,
+  },
+  logoutButtonView: {
+    width: width*0.15,
+    marginLeft: width*0.165,
+  },
 });
 
 AppRegistry.registerComponent('MobileMolecularWeight', () => ChemistryApp);
